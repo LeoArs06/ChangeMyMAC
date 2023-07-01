8ee3d10485c4974ec8f24b551a98dfbbb8c07478
Ares <67775874+LeoArs06@users.noreply.github.com>
2023-07-01 23:24:00 +0200

    Update setup.py

--- MAC-OS/setup.py
+++ MAC-OS/setup.py
@@ -9,6 +9,18 @@ setup(
     options={
         'py2app': {
             'iconfile': ICON,
+            'plist': {
+                'CFBundleDisplayName': 'changemymac',
+                'CFBundleName': 'Change my MAC',
+                'CFBundleIdentifier': 'com.naitshiro.changemymac',
+                'CFBundleShortVersionString': '1.0',
+                'CFBundleVersion': '1.0',
+                'LSApplicationCategoryType': 'public.app-category.utilities',
+                'LSUIElement': '1',
+                'NSPrincipalClass': 'NSApplication',
+                'LSBackgroundOnly': '1',
+                'Developers': 'LeoArs06, Natisfaction'
+            }
         }
     }
 )
