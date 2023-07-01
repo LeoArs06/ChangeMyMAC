fd33998699ad01ef743a51db3fc4afb4dba92a67
Natisfaction <cl406761@gmail.com>
2023-07-01 19:20:34 +0200

    Second commit

---
+++ src/ChangeMyMac.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+
+def main():
+    root = tk.Tk()
+    root.title("Change My Mac")
+    root.geometry("600x400")
+    root.configure(background="blue")
+    root.mainloop()
+
+main()
