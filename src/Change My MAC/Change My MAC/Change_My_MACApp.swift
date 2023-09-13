19ee8cfedf08454a794cf8235b559f462dd00e61
Natisfaction <cl406761@gmail.com>
2023-09-13 12:42:40 +0200

    Added xcode project

---
+++ src/Change My MAC/Change My MAC/Change_My_MACApp.swift
@@ -0,0 +1,17 @@
+//
+//  Change_My_MACApp.swift
+//  Change My MAC
+//
+//  Created by Christian Leone on 13/09/23.
+//
+
+import SwiftUI
+
+@main
+struct Change_My_MACApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
