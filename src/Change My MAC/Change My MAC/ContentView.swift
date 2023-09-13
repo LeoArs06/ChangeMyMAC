cde47235192181ceb40832629702a688d4fbe75c
Natisfaction <cl406761@gmail.com>
2023-09-13 12:42:40 +0200

    Commit

---
+++ src/Change My MAC/Change My MAC/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Change My MAC
+//
+//  Created by Christian Leone on 13/09/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
