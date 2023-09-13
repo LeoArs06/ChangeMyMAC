d8491dd4057f4822bee7f73916647976e1897d66
Natisfaction <cl406761@gmail.com>
2023-09-13 21:54:44 +0200

    Commit

--- src/ChangeMyMAC/ChangeMyMAC/ChangeMyMACApp.swift
+++ src/ChangeMyMAC/ChangeMyMAC/ChangeMyMACApp.swift
@@ -1,17 +1,66 @@
-//
-//  ChangeMyMACApp.swift
-//  ChangeMyMAC
-//
-//  Created by Christian Leone on 13/09/23.
-//
-
 import SwiftUI
+import ServiceManagement
 
 @main
-struct ChangeMyMACApp: App {
+struct UtilityApp: App {
+    
     var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+        MenuBarExtra("Change My MAC", systemImage: "circle") {
+            AppMenu()
+        }.menuBarExtraStyle(.window)
+        
+        WindowGroup{}
     }
+}
+
+struct AppMenu: View {
+    @State private var mac_add: String = ""
+    @State private var interface: String = ""
+    
+    var body: some View {
+        VStack {
+            Text("Change My MAC").padding(.bottom)
+            
+            HStack {
+                Text("Network Interface:  ")
+                Menu {
+                    Button(action: set_interface) {
+                        Text("Interface")
+                    }
+                } label: {
+                    Text("Network interface")
+                }
+            }
+            .padding(.bottom)
+            
+            HStack {
+                Text("New MAC Address: ")
+                TextField("New MAC Address here...", text: $mac_add).textFieldStyle(RoundedBorderTextFieldStyle())
+            }
+            
+            Divider().padding()
+            
+            HStack {
+                Button(action: randomize) {
+                    Text("Random MAC").padding()
+                }
+                Button(action: update_mac) {
+                    Text("Update MAC‎").padding()
+                }
+            }
+        }
+        .padding()
+    }
+}
+
+func randomize() {
+    // Randomize the MAC
+}
+
+func update_mac() {
+    // Update the MAC (maybe authenticate???)
+}
+
+func set_interface() {
+    // Set the interface
 }
