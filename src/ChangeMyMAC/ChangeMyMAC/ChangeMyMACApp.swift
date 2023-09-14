c07c8ace5386b7d3051636983d477f2fbacdb26b
Natisfaction <cl406761@gmail.com>
2023-09-14 13:29:58 +0200

    Random function, list of interfaces

--- src/ChangeMyMAC/ChangeMyMAC/ChangeMyMACApp.swift
+++ src/ChangeMyMAC/ChangeMyMAC/ChangeMyMACApp.swift
@@ -1,66 +1,140 @@
 import SwiftUI
 import ServiceManagement
+import SystemConfiguration
+
+import Darwin
+import Foundation
 
 @main
-struct UtilityApp: App {
-    
+struct ChangeMyMAC: App {
     var body: some Scene {
         MenuBarExtra("Change My MAC", systemImage: "circle") {
             AppMenu()
-        }.menuBarExtraStyle(.window)
-        
-        WindowGroup{}
+        }
+        .menuBarExtraStyle(.window)
+        WindowGroup {}
     }
 }
 
 struct AppMenu: View {
+    // MAC Address and selecter interface
     @State private var mac_add: String = ""
-    @State private var interface: String = ""
+    @State private var sInterface: String = ""
     
+    // Network Interfaces
+    private var interfaces: [String] {
+        getInterfaces()
+    }
+
     var body: some View {
         VStack {
-            Text("Change My MAC").padding(.bottom)
+            // Title (I'll format the string better later)...
+            Text("Change My MAC")
+                .padding(.bottom)
             
             HStack {
-                Text("Network Interface:  ")
+                Text("Network Interface: ")
+                
                 Menu {
-                    Button(action: set_interface) {
-                        Text("Interface")
+                    // This snipped simply creates buttons with the interface name
+                    ForEach(interfaces, id: \.self) { interface in
+                        Button(action: {
+                            sInterface = interface
+                        }) {
+                            Text(interface)
+                        }
                     }
                 } label: {
-                    Text("Network interface")
+                    Text(sInterface.isEmpty ? "Network interface" : sInterface)
                 }
             }
             .padding(.bottom)
             
             HStack {
                 Text("New MAC Address: ")
-                TextField("New MAC Address here...", text: $mac_add).textFieldStyle(RoundedBorderTextFieldStyle())
+                
+                // MAC Address field
+                TextField("New MAC Address here...", text: $mac_add)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
             }
             
-            Divider().padding()
+            Divider()
+                .padding()
             
             HStack {
                 Button(action: randomize) {
-                    Text("Random MAC").padding()
+                    Text("Random MAC")
+                        .padding()
                 }
+                
                 Button(action: update_mac) {
-                    Text("Update MAC‎").padding()
+                    Text("Update MAC")
+                        .padding()
                 }
             }
         }
         .padding()
     }
-}
+    
+    func printa(cfArray: CFArray) {
+        print(cfArray)
+    }
 
-func randomize() {
-    // Randomize the MAC
-}
+    func randomize() {
+        let mac_nibble: [Character] = Array("0123456789abcdef")
+        var newmac: String = ""
+        
+        // First nibble random
+        if let randomNibble = mac_nibble.randomElement() {
+            newmac.append(randomNibble)
+        }
+        
+        // Second nibble ONLY odd
+        if let randomNibble = ["0", "2", "4", "6", "8", "a", "c", "e"].randomElement() {
+            newmac.append(randomNibble)
+        }
+        
+        // MAC Address bytes
+        for _ in 0...4 {
+            newmac.append(":")
+            for _ in 0...1 {
+                if let randomNibble = mac_nibble.randomElement() {
+                    newmac.append(randomNibble)
+                }
+            }
+        }
+        
+        mac_add = newmac
+    }
+    
+    func getInterfaces() -> [String] {
+        var ifaddr: UnsafeMutablePointer<ifaddrs>? = nil
+        var sortedNames: [String] = []
+        
+        if getifaddrs(&ifaddr) == 0 {
+            var ptr = ifaddr
+            
+            // Remove duplicates
+            var interfaceNames = Set<String>()
+            
+            while ptr != nil {
+                if let interfaceName = String(cString: (ptr?.pointee.ifa_name)!).components(separatedBy: ":").first {
+                    interfaceNames.insert(interfaceName)
+                }
+                
+                ptr = ptr?.pointee.ifa_next
+            }
+            
+            freeifaddrs(ifaddr)
+            
+            // Sort them alphabetically
+            sortedNames = interfaceNames.sorted()
+        }
+        
+        return sortedNames
+    }
 
-func update_mac() {
-    // Update the MAC (maybe authenticate???)
-}
-
-func set_interface() {
-    // Set the interface
+    func update_mac() {
+        // Update the MAC (maybe authenticate???)
+    }
 }
