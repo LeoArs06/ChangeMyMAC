dad0a0e01b2da1a5b16f080eb4c7f9f845539e8a
LeoArs06 <leoars03@gmail.com>
2023-09-20 18:06:23 +0200

    Create c# app for windows

---
+++ ChangeMyMac/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace ChangeMyMac
+{
+    partial class Form1 : Form
+    {
+        private TextBox macAddressTextBox;
+        private ComboBox networkInterfaceComboBox;
+        private Button randomizeButton;
+        private Button updateMacButton;
+
+
+        private void InitializeComponent()
+        {
+            macAddressTextBox = new TextBox();
+            networkInterfaceComboBox = new ComboBox();
+            randomizeButton = new Button();
+            updateMacButton = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            SuspendLayout();
+            // 
+            // macAddressTextBox
+            // 
+            macAddressTextBox.Location = new Point(174, 139);
+            macAddressTextBox.Name = "macAddressTextBox";
+            macAddressTextBox.Size = new Size(177, 23);
+            macAddressTextBox.TabIndex = 0;
+            // 
+            // networkInterfaceComboBox
+            // 
+            networkInterfaceComboBox.Location = new Point(174, 81);
+            networkInterfaceComboBox.Name = "networkInterfaceComboBox";
+            networkInterfaceComboBox.Size = new Size(177, 23);
+            networkInterfaceComboBox.TabIndex = 1;
+            // 
+            // randomizeButton
+            // 
+            randomizeButton.Location = new Point(62, 195);
+            randomizeButton.Name = "randomizeButton";
+            randomizeButton.Size = new Size(90, 30);
+            randomizeButton.TabIndex = 2;
+            randomizeButton.Text = "Random MAC";
+            randomizeButton.Click += RandomizeButton_Click;
+            // 
+            // updateMacButton
+            // 
+            updateMacButton.Location = new Point(261, 195);
+            updateMacButton.Name = "updateMacButton";
+            updateMacButton.Size = new Size(90, 30);
+            updateMacButton.TabIndex = 3;
+            updateMacButton.Text = "Update MAC";
+            updateMacButton.Click += UpdateMacButton_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = false;
+            label1.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            label1.Location = new System.Drawing.Point(137, 24);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(178, 30);
+            label1.TabIndex = 4;
+            label1.Text = "Change My MAC";
+            label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(62, 84);
+            label2.Name = "label2";
+            label2.Size = new Size(104, 15);
+            label2.TabIndex = 5;
+            label2.Text = "Network Interface:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(62, 147);
+            label3.Name = "label3";
+            label3.Size = new Size(109, 15);
+            label3.TabIndex = 6;
+            label3.Text = "New MAC Address:";
+            // 
+            // Form1
+            // 
+            ClientSize = new Size(444, 378);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(macAddressTextBox);
+            Controls.Add(networkInterfaceComboBox);
+            Controls.Add(randomizeButton);
+            Controls.Add(updateMacButton);
+            Name = "Form1";
+            Text = "Change My MAC";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        private void InitializeUI()
+        {
+            // Populate the network interface ComboBox with available interfaces
+            string[] interfaces = GetNetworkInterfaces();
+            networkInterfaceComboBox.Items.AddRange(interfaces);
+
+            if (interfaces.Length > 0)
+            {
+                networkInterfaceComboBox.SelectedIndex = 0;
+            }
+        }
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+    }
+
+}
---
+++ ChangeMyMac/Form1.cs
@@ -0,0 +1,132 @@
+using System;
+using System.Windows.Forms;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Diagnostics;
+
+namespace ChangeMyMac
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private string[] GetNetworkInterfaces()
+        {
+            try
+            {
+                // Esegui il comando PowerShell per ottenere l'elenco delle interfacce di rete
+                ProcessStartInfo psi = new ProcessStartInfo
+                {
+                    FileName = "powershell",
+                    Arguments = "Get-NetAdapter | Select-Object -ExpandProperty Name",
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true
+                };
+
+                using (Process process = new Process())
+                {
+                    process.StartInfo = psi;
+                    process.Start();
+                    string output = process.StandardOutput.ReadToEnd();
+                    process.WaitForExit();
+
+                    // Estrai i nomi delle interfacce dalla risposta
+                    string[] interfaces = output.Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+
+                    return interfaces;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Errore durante il recupero delle interfacce di rete: {ex.Message}", "Errore", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return new string[0]; // Restituisce un array vuoto in caso di errore
+            }
+        }
+        private string GenerateRandomMacAddress()
+        {
+            Random rand = new Random();
+            byte[] macAddr = new byte[6];
+            rand.NextBytes(macAddr);
+
+            // Set first bit as "unicast" address
+            macAddr[0] = (byte)(macAddr[0] & (byte)254);
+
+            macAddr[0] = (byte)(macAddr[0] | (byte)2);
+
+            return string.Join(":", macAddr.Select(b => b.ToString("X2")));
+        }
+
+
+        private void RandomizeButton_Click(object sender, EventArgs e)
+        {
+            string randomMac = GenerateRandomMacAddress();
+            macAddressTextBox.Text = randomMac;
+        }
+
+        private void UpdateMacButton_Click(object sender, EventArgs e)
+        {
+            string selectedInterface = networkInterfaceComboBox.SelectedItem as string;
+            string newMacAddress = macAddressTextBox.Text.Replace(":", "-");
+
+            if (!string.IsNullOrEmpty(selectedInterface) && !string.IsNullOrEmpty(newMacAddress))
+            {
+                if (ChangeMacAddress(selectedInterface, newMacAddress) == 0)
+                {
+                    MessageBox.Show("MAC Address updated successfully", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show("Can't update current MAC Address", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please select an interface and provide a valid MAC address.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private int ChangeMacAddress(string interfaceName, string newMacAddress)
+        {
+            if (string.IsNullOrEmpty(interfaceName) || string.IsNullOrEmpty(newMacAddress))
+            {
+                MessageBox.Show("Si prega di selezionare un'interfaccia e fornire un indirizzo MAC valido.", "Errore", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return 1;
+            }
+
+            newMacAddress = newMacAddress.Replace(":", "-");
+            ProcessStartInfo psi = new ProcessStartInfo
+            {
+                FileName = "powershell",
+                Arguments = $"-Command Set-NetAdapter -Name {interfaceName} -MacAddress {newMacAddress} -Confirm:$false",
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                UseShellExecute = true, // Utilizza ShellExecute per richiedere i privilegi di amministratore quando necessario
+                CreateNoWindow = true
+            };
+
+            using (Process process = new Process())
+            {
+                process.StartInfo = psi;
+                process.Start();
+                process.WaitForExit();
+
+                string output = process.StandardOutput.ReadToEnd();
+                string error = process.StandardError.ReadToEnd();
+
+                if (process.ExitCode == 0)
+                {
+                    MessageBox.Show("Indirizzo MAC aggiornato con successo", "Successo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    return 0;
+                }
+                else
+                {
+                    MessageBox.Show($"Impossibile aggiornare l'indirizzo MAC corrente: {error}", "Errore", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return 1;
+                }
+            }
+        }
+    }
+}
